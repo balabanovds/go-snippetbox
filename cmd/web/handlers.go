3c4e7ee80908b3e6aec6a71d7e19a1d27200f68a
Denis Balabanov <balabanov.ds@gmail.com>
2020-05-06 09:15:18 +0300

    add snippets actions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/balabanovds/go-snippetbox/pkg/models"
 	"net/http"
 	"strconv"
 )
@@ -13,22 +14,32 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/templates/home.page.html",
-		"./ui/templates/base.layout.html",
-		"./ui/templates/footer.partial.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, s := range snippets {
+		fmt.Fprintf(w, "%v\n\n", s)
 	}
+
+	//files := []string{
+	//	"./ui/templates/home.page.html",
+	//	"./ui/templates/base.layout.html",
+	//	"./ui/templates/footer.partial.html",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//
+	//err = ts.Execute(w, nil)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -37,7 +48,17 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "consuming snippet with id %d..", id)
+
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+			return
+		}
+		app.serverError(w, err)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "%v", s)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/balabanovds/go-snippetbox/pkg/models"
 )
 
@@ -16,15 +17,67 @@ func NewSnippetModel(db *sql.DB) *SnippetModel {
 
 // Insert new record
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt, err := m.DB.Prepare("INSERT INTO snippets (title, content, created, expires) " +
+		"VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))")
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := stmt.Exec(title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // Get one record by id
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	row := m.DB.QueryRow("SELECT id, title, content, created, expires FROM snippets "+
+		"WHERE expires > UTC_TIMESTAMP AND id = ?", id)
+
+	s := &models.Snippet{}
+
+	if err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		}
+		return nil, err
+	}
+	return s, nil
 }
 
 // Latest will return last 10 records
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	rows, err := m.DB.Query("SELECT id, title, content, created, expires FROM snippets " +
+		"WHERE expires > UTC_TIMESTAMP ORDER BY created DESC LIMIT 10")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var snippets []*models.Snippet
+
+	for rows.Next() {
+		s := &models.Snippet{}
+
+		if err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires); err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
